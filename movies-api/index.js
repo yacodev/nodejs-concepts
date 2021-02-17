e8c417519a95b7b0c6dec5b8d5c86ae3cee830b0
Carlos yaco <cyaco33@gmail.com>
2021-02-16 21:38:14 -0500

    developed movie API

--- movies-api/index.js
+++ movies-api/index.js
@@ -2,9 +2,16 @@ const express = require('express');
 const app = express();
 const {config} = require('./config/index');
 const moviesApi = require('./routes/movies');
+
+const {logError, errorHandler}= require('./utils/middleware/errorHandlers')
+
 //body parser
 app.use(express.json());
+
 moviesApi(app);
+//middleware de error siempre tienen que ir al final de las rutas
+app.use(logError);
+app.use(errorHandler);
 
 app.listen(config.port, function(){
   console.log(`listening http://localhost:${config.port}`)
